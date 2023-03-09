3165bc21353dee4b97c8c1f6b2469130afad43e3
CarlaLSC <118780467+CarlaLSC@users.noreply.github.com>
2023-03-09 08:14:18 -0300

    aa

--- dados.php
+++ dados.php
@@ -1,19 +1,20 @@
 <?php
-$nome=$_POST['nome'];
-$telefone=$_POST['telefone'];
-$cpf=$_POST['cpf'];
-$Email=$_POST['email'];
-$dataNascimento=$_POST['data'];
-$escolaridade=$_POST['escolaridade'];
-$sexo=$_POST['sexo'];
+    $nome=$_POST['nome'];
+    $sobrenome=$_POST['sobrenome'];
+    $email=$_POST['email'];
+    $usuario=$_POST['usuario'];
+    $nivel=$_POST['nivel'];
+    $conteudo=$_POST['conteudo'];
+    $senha= $_POst['senha'];
 
-$CONN= NEW MySQLi('localhost','root', '' ,'bd_empresa');
-if($CONN->connect_error){
-    die("Falha de conexão".$CONN->connect_error);}
-    $SQL="insert into funcionario(nome,cpf,dataNascimento,Email,escolaridade,sexo,telefone)
-    VALUES('$nome','$cpf','$dataNascimento','$Email','$escolaridade','$sexo','$telefone')";
+    $CONN= NEW MySQLi('localhost','root', '' ,'bd_empresa');
+    if($CONN->connect_error){
+        die("Falha de conexão".$CONN->connect_error);}
+
+    $SQL="insert into aludo(nome, sobrenome, email, usuario, nivel, conteudo, senha)
+    VALUES('$nome', '$sobrenome', '$email', '$usuario', '$nivel', '$conteudo', '$senha')";
 
     mysqli_query($CONN,$SQL);
     mysqli_close($CONN);
 
-    ?>
+?>
