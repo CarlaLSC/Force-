17b64f86be191da96ca5f4da429738563dadd41a
CarlaLSC <118780467+CarlaLSC@users.noreply.github.com>
2023-03-29 08:54:16 -0300

    Create select_usuario.php

---
+++ select_usuario.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+
+include_once("ConexaoKW.php");
+
+
+$SQL = "SELECT * from aluno  where nome = '$nome' and senha = '$senha'";
+
+$result = mysqli_query($CONN, $SQL);
+$linha = mysqli_fetch_array($result);
+
+if($linha){
+    
+    echo 'Usuario aceito';
+    
+   // $url = 'Location:./loginKW.html?msg=s';
+    
+}else{
+
+    echo'Usuario não aceito';
+
+    //$url = 'Location:./loginKW.html?msg=n';
+}
+    
+//header($url);
+//mysqli_query($conn, $sql);
+mysqli_close($CONN);
+
+?>
